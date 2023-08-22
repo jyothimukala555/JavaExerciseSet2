7660c424585b32ca8612fe03ed00113fc8b46d1d
jyothimukala555 <138197142+jyothimukala555@users.noreply.github.com>
2023-08-22 23:03:25 +0530

    Create DimensionFeetInches.java

---
+++ DimensionFeetInches.java
@@ -0,0 +1,41 @@
+package com.javaExSet2Opps;
+
+public class Dimension {
+
+
+	    private int feet;
+	    private int inches;
+	    
+	    public Dimension(int inches) {
+	 
+	      if(inches<0){
+	      this.feet=-1;
+	      this.inches=-1;
+	      }
+	      else
+	      {
+	      this.feet = inches/12;
+	      this.inches =inches%12;
+	      } 
+	    }
+	    
+	    
+	    public int getFeet() {
+	   return feet;
+	   
+	        
+	    }
+	    
+	    public int getInches() {
+	        return inches;
+	        
+	    }    
+	
+ public static void main (String [] args) {
+	 Dimension dim = new Dimension(25);
+			 
+	   System.out.println(dim.getFeet());
+	   System.out.println(dim.getInches());
+	  
+ }
+}
